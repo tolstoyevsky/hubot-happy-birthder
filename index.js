d2d4dcc439ece5e6b1763851a015c25fb7226aef
Eugene Hatsko <e.chatzko@wis.eu.com>
2018-07-16 16:02:52 +0300

    Docstring was moved into head of file

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
+// Description:
+//   Hubot script for writing birthday messages to users. It uses Tenor GIFs to make the messages more lively.
+//
+// Commands:
+//   list birthdays - shows a list of users and their birthdays
+//   birthday set <username> <date>/<month>/<year> - sets a birthday for the user
+//   birthdays on <date>/<month>/<year> - shows a list of users with a set birthday date
+//
+// Author:
+//   6r1d
 (function () {
-    // Description:
-    //   Hubot script for writing birthday messages to users. It uses Tenor GIFs to make the messages more lively.
-    //
-    // Commands:
-    //   list birthdays - shows a list of users and their birthdays
-    //   birthday set <username> <date>/<month>/<year> - sets a birthday for the user
-    //   birthdays on <date>/<month>/<year> - shows a list of users with a set birthday date
-    //
-    // Author:
-    //   6r1d
-
     const schedule = require('node-schedule');
     const moment = require('moment');
     const n_fetch = require('node-fetch');
