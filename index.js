dee9a2fe8dcdf5a032ff96630bb66f7e812dea8f
Eugene Hatsko <e.chatzko@wis.eu.com>
2018-07-17 09:14:11 +0300

    Fix for index.js * script was regenerated from coffee script file

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-module.exports = function (robot, scripts) {
-    let scriptsPath;
-    scriptsPath = path.resolve(__dirname, 'src');
-    return fs.exists(scriptsPath, function (exists) {
-        let i, len, ref, results, script;
-        if (exists) {
-            ref = fs.readdirSync(scriptsPath);
-            results = [];
-            for (i = 0, len = ref.length; i < len; i++) {
-                script = ref[i];
-                if ((scripts != null) && indexOf.call(scripts, '*') < 0) {
-                    if (indexOf.call(scripts, script) >= 0) {
-                        results.push(robot.loadFile(scriptsPath, script));
+(function () {
+    var fs, path,
+        indexOf = [].indexOf;
+
+    fs = require('fs');
+
+    path = require('path');
+
+    module.exports = function (robot, scripts) {
+        let scriptsPath;
+        scriptsPath = path.resolve(__dirname, 'src');
+        return fs.exists(scriptsPath, function (exists) {
+            let i, len, ref, results, script;
+            if (exists) {
+                ref = fs.readdirSync(scriptsPath);
+                results = [];
+                for (i = 0, len = ref.length; i < len; i++) {
+                    script = ref[i];
+                    if ((scripts != null) && indexOf.call(scripts, '*') < 0) {
+                        if (indexOf.call(scripts, script) >= 0) {
+                            results.push(robot.loadFile(scriptsPath, script));
+                        } else {
+                            results.push(void 0);
+                        }
                     } else {
-                        results.push(void 0);
+                        results.push(robot.loadFile(scriptsPath, script));
                     }
-                } else {
-                    results.push(robot.loadFile(scriptsPath, script));
                 }
+                return results;
             }
-            return results;
-        }
-    });
-};
+        });
+    };
+
+}).call(this);
