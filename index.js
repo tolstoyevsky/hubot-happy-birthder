d4b68199bfeff9b82e6727f37fea89b9088fc38f
Evgeny Golyshev <eugulixes@gmail.com>
2018-08-13 17:08:14 +0300

    Do not overcomplicate index.js

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-(function () {
-    var fs, path,
-        indexOf = [].indexOf;
+'use strict'
 
-    fs = require('fs');
+const path = require('path')
 
-    path = require('path');
+module.exports = (robot) => {
+    const scriptsPath = path.resolve(__dirname, 'src')
+    robot.loadFile(scriptsPath, 'birthder.js')
+}
 
-    module.exports = function (robot, scripts) {
-        let scriptsPath;
-        scriptsPath = path.resolve(__dirname, 'src');
-        return fs.exists(scriptsPath, function (exists) {
-            let i, len, ref, results, script;
-            if (exists) {
-                ref = fs.readdirSync(scriptsPath);
-                results = [];
-                for (i = 0, len = ref.length; i < len; i++) {
-                    script = ref[i];
-                    if ((scripts != null) && indexOf.call(scripts, '*') < 0) {
-                        if (indexOf.call(scripts, script) >= 0) {
-                            results.push(robot.loadFile(scriptsPath, script));
-                        } else {
-                            results.push(void 0);
-                        }
-                    } else {
-                        results.push(robot.loadFile(scriptsPath, script));
-                    }
-                }
-                return results;
-            }
-        });
-    };
-
-}).call(this);
