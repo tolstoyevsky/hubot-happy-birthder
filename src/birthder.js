3e1673fb226e491e2cadb739f508c6099a01f8e9
Anton <antonio.maksimovich@gmail.com>
2018-09-12 18:19:57 +0300

    Move quotes to file

--- src/birthder.js
+++ src/birthder.js
@@ -15,6 +15,8 @@
     const schedule = require('node-schedule');
     const moment = require('moment');
     const nFetch = require('node-fetch');
+    const path = require('path')
+    const fs = require('fs');
 
     const TENOR_API_KEY = process.env.TENOR_API_KEY || "";
     const TENOR_IMG_LIMIT = process.env.TENOR_IMG_LIMIT || 50;
@@ -38,28 +40,8 @@
     // See https://momentjs.com/docs/#/displaying/ for details.
     const OUTPUT_SHORT_DATE_FORMAT = "DD.MM";
 
-    const QUOTES = [
-        "Hoping that your day will be as special as you are.",
-        "Count your life by smiles, not tears. Count your age by friends, not years.",
-        "May the years continue to be good to you. Happy Birthday!",
-        "You're not getting older, you're getting better.",
-        "May this year bring with it all the success and fulfillment your heart desires.",
-        "Wishing you all the great things in life, hope this day will bring you an extra share of all that makes you happiest.",
-        "Happy Birthday, and may all the wishes and dreams you dream today turn to reality.",
-        "May this day bring to you all things that make you smile. Happy Birthday!",
-        "Your best years are still ahead of you.",
-        "Birthdays are filled with yesterday's memories, today's joys, and tomorrow's dreams.",
-        "Hoping that your day will be as special as you are.", "You'll always be forever young.",
-        "Happy Birthday, you're not getting older, you're just a little closer to death.",
-        "Birthdays are good for you. Statistics show that people who have the most live the longest!",
-        "I'm so glad you were born, because you brighten my life and fill it with joy.",
-        "Always remember: growing old is mandatory, growing up is optional.",
-        "Better to be over the hill than burried under it.",
-        "You always have such fun birthdays, you should have one every year.",
-        "Happy birthday to you, a person who is smart, good looking, and funny and reminds me a lot of myself.",
-        "We know we're getting old when the only thing we want for our birthday is not to be reminded of it.",
-        "Happy Birthday on your very special day, I hope that you don't die before you eat your cake."
-    ];
+    const QUOTES_PATH = path.join(__dirname, '/quotes.txt');
+    const QUOTES = fs.readFileSync(QUOTES_PATH, 'utf8').toString().split("\n");
 
     /**
      * Use API to check if the specified user is admin.
